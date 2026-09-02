1af72e1c08ee48bb341ad3cb8cdd5c2159692154
yoghurtfork <limyt176@gmail.com>
2026-09-02 20:37:41 +0800

    Export LoRA utils

--- mace/modules/__init__.py
+++ mace/modules/__init__.py
@@ -34,6 +34,14 @@ from .loss import (
     WeightedEnergyForcesNacsDipoleLoss,
     InvariantsWeightedEnergyForcesNacsDipoleLoss,
 )
+from .lora import (
+    LoRADenseLinear,
+    LoRAFCLayer,
+    LoRAO3Linear,
+    inject_lora,
+    inject_LoRAs,
+    merge_lora_weights,
+)
 from .models import (
     ExcitedMACE,
     AutoencoderExcitedMACE,
@@ -113,4 +121,10 @@ __all__ = [
     "PermutationInvariantEncoder",
     "AutoencoderExcitedMACE",
     "InvariantsWeightedEnergyForcesNacsDipoleLoss",
+    "LoRADenseLinear",
+    "LoRAFCLayer",
+    "LoRAO3Linear",
+    "inject_lora",
+    "inject_LoRAs",
+    "merge_lora_weights",
 ]
